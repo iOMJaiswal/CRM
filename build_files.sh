1b1782976733ea6117c2f15d316c7f9dcc894343
OM JAISWAL <95462129+iOMJaiswal@users.noreply.github.com>
2024-04-27 18:20:27 +0530

    changed

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
 echo "Building the Project"
+pip install -r requirements.txt
 
 echo "Make Migrations"
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static Files"
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
