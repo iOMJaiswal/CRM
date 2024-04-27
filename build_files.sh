9f47e5a9a10359dac04473ab32628a7a7bd8e62d
OM JAISWAL <95462129+iOMJaiswal@users.noreply.github.com>
2024-04-27 18:14:43 +0530

    Chaged Build

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 echo "Building the Project"
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static Files"
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
